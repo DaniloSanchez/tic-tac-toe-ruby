f948489f1d73ced9aeb1559a8ded52571657a821
Danilo <pernix@ubuntu.(none)>
2013-12-12 14:46:20 -0600

    validating the movement

--- Dashboard.rb
+++ Dashboard.rb
@@ -1,19 +1,31 @@
-require 'matrix'
-
 class Dashboard
 
   def initialize
-    @dashboard = Matrix[ [25, 93, 89], 
-                         [-1, 66, 43],
-                         [34, 53, 64]]
+    @machine = '_X_'
+    @human = '_O_'
+    @empty = '___'
+    @dashboard =[ [@empty, @empty, @empty],
+                  [@empty, @empty, @empty],
+                  [@empty, @empty, @empty] ]
   end
 
   def print_dashboard
-    @dashboard.each_slice(@dashboard.column_size) {|row| p row }
+    @dashboard.each { |row| p row }
+  end
+
+  def position_is_valid?(position)
+    @dashboard[position.first][position.last] == @empty
+  end
+
+  def do_movement(user, position)
+    @dashboard[position.first][ position.last] = (user ==  0 ? @machine : @human)
   end
 
 end
 
 
 i = Dashboard.new
+i.print_dashboard
+_pos = [1,1]
+i.do_movement(1,_pos) if i.position_is_valid?(_pos)
 i.print_dashboard
