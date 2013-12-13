0b5bb9a5811ef7035b6aac1f34247c27d3c1382d
DaniloSanchez <dasmdanilo@gmail.com>
2013-12-13 16:13:56 -0600

    validating row and column

--- Dashboard.rb
+++ Dashboard.rb
@@ -4,9 +4,9 @@ class Dashboard
     @machine = '_X_'
     @human = '_O_'
     @empty = '___'
-    @dashboard = [ [@human, @human,  @human],
-                   [@machine, @machine, @human],
-                   [@human, @machine, @machine] ]
+    @dashboard = [ [@empty, @empty, @empty],
+                   [@empty, @empty, @empty],
+                   [@empty, @empty, @empty] ]
   end
 
   def print_dashboard
@@ -18,39 +18,50 @@ class Dashboard
   end
 
   def do_movement(user, position)
-    @dashboard[position.first][ position.last] = (user ==  0 ? @machine : @human)
+    @dashboard[position.first][position.last] = (user ==  0 ? @machine : @human)
   end
 
   def movements_exist?
-    @dashboard.any? do
-      |elem| elem.any? { |cell| cell == @empty }
-    end
+    @dashboard.any? { |elem| elem.any? { |cell| cell == @empty } }
   end
 
-  def row?
+  def check_rows
     @dashboard.map.any? do
       |row| row.all? { |elem| elem == @human } ||
         row.all? { |elem| elem == @machine }
     end
   end
 
-  def column?
+  def check_columns
     transpose_dashboard
-    result = row?
+    result = check_rows
     transpose_dashboard
     result
+  end
+
+  def check_diagonal
+    (@dashboard[0][0] && @dashboard[1][1] && @dashboard[2][2]) == @human ||
+      (@dashboard[0][0] && @dashboard[1][1] && @dashboard[2][2]) == @machine
+  end
+
+  def check_inverse_diagonal
+
   end
 
   private
 
   def transpose_dashboard
     temporal_dashboard = [[],[],[]]
+    @dashboard = transpose_dashboard_aux(temporal_dashboard)
+  end
+
+  def transpose_dashboard_aux(temporal_dashboard)
     @dashboard.map do 
       |row| temporal_dashboard.first << row.first
         temporal_dashboard[1] << row[1]
         temporal_dashboard.last << row.last
     end
-    @dashboard = temporal_dashboard
+    temporal_dashboard
   end
 
 end
@@ -62,6 +73,7 @@ i.print_dashboard
 #i.do_movement(1,_pos) if i.position_is_valid?(_pos)
 #i.print_dashboard
 #i.movements_exist
-p i.row?
-p i.column?
+p i.check_rows
+p i.check_columns
 i.print_dashboard
+p i.check_diagonal
