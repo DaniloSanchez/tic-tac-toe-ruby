dfaf8e4d6eb48af8e3f2450199bfe629f25a6282
DaniloSanchez <dasmdanilo@gmail.com>
2013-12-16 11:53:45 -0600

    Refactor code

--- brain.rb
+++ brain.rb
@@ -11,28 +11,33 @@ class Brain
   def strat_game
     @imessages.message_welcome
     @imessages.message_instructions
-    @idashboard.print_dashboard
+    @turn = 0
+    @player = ''
     start
   end
 
   def start
-    turn = 0
-    player = ''
-    while @idashboard.movements_exist? == true || @idashboard.check_status == false
-      player = turn.even? ? "Player 1" : "Player 2"
-      in_process(player)
-      turn = turn += 1
+    while @idashboard.movements_exist? == true && @idashboard.check_status == false
+      in_process
     end
-    congratulations(player)
+    congratulations
   end
 
-  def in_process(player)
+  def in_process
+    @player = @turn.even? ? "Player 1" : "Player 2"
+    in_process_auxiliar
+    @turn = @turn += 1
+  end
+
+
+  def in_process_auxiliar
     @idashboard.print_dashboard
-    @imessages.message_next_player(player)
+    @imessages.message_next_player(@player)
     loop do
       pos_x = position_row
       pos_y = position_column
-      break unless @idashboard.do_movement(player, [pos_x, pos_y]) if @idashboard.position_is_valid?([pos_x,_pox_y])
+      break @idashboard.do_movement(@player, [pos_x.to_i, pos_y.to_i]) if @idashboard.position_is_valid?([pos_x.to_i,pos_y.to_i])
+      @imessages.message_invalid_movement
     end
   end
 
@@ -46,11 +51,13 @@ class Brain
     gets.chomp
   end
 
-  def congratulations(player)
-    @idashboard.check_status ? @imessages.message_congratulations(player) : @imessages.message_draw
+  def congratulations
+    @idashboard.print_dashboard
+    @idashboard.check_status ? @imessages.message_congratulations(@player) : @imessages.message_draw
   end
 
 end
 
 game = Brain.new
 game.strat_game
+
